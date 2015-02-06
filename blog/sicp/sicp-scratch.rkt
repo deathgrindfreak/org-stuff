c525d273e8117c3e313e1b0a80d99a8a86780929
Cooper Bell <john.cooper.bell@gmail.com>
2015-02-05 21:25:02 -0600

    added some exercises

--- blog/sicp/sicp-scratch.rkt
+++ blog/sicp/sicp-scratch.rkt
@@ -36,3 +36,71 @@
       (loop (+ i 1) (f guess guess-factor) (/ x 10)))))
 
 (print-comparisons 0.5 2 / 0.2)
+
+
+(define (paint-outline frame)
+  (let ((origin (0.0 0.0))
+        (edge1 (1.0 0.0))
+        (edge2 (0.0 1.0))
+        (opposite-origin (1.0 1.0)))
+    ((segments->painter (list (make-segment origin edge1)
+                              (make-segment origin edge2)
+                              (make-segment edge1 opposite-origin)
+                              (make-segment edge2 opposite-origin)))
+     frame)))
+
+(define (paint-x frame)
+  (let ((origin (0.0 0.0))
+        (edge1 (1.0 0.0))
+        (edge2 (0.0 1.0))
+        (opposite-origin (1.0 1.0)))
+    ((segments->painter (list (make-segment origin opposite-origin)
+                              (make-segment edge1 edge2)))
+     frame)))
+
+
+(define (paint-diamond frame)
+  (let ((origin-edge1 (0.0 0.5))
+        (origin-edge2 (0.5 0.0))
+        (edge1-opp (0.5 1.0))
+        (edge2-opp (1.0 0.5)))
+    ((segments->painter (list (make-segment origin-edge1 origin-edge2)
+                              (make-segment origin-edge1 edge1-opp)
+                              (make-segment origin-edge2 edge2-opp)
+                              (make-segment edge1-opp edge2-opp)))
+     frame)))
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((painter-below (transform-painter painter 1
+                                            (make-vect 0.0 0.0)
+                                            (make-vect 1.0 0.0)
+                                            split-point))
+          (painter-above (transform-painter painter2
+                                            split-point
+                                            (make-vect 1.0 0.5)
+                                            (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (painter-below frame)
+        (painter-above frame)))))
+
+(define (below painter1 painter2)
+  (rotate-270 (beside painter1 painter2)))
